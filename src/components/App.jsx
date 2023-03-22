d5fdeb591a51ff5cb4ae094a522670f2ed98caa2
eugeniusz57 <104994034+eugeniusz57@users.noreply.github.com>
2023-03-22 18:36:40 +0100

    create project
    instal all libery and add globl style

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+export const App = () => {
+  return (
+    <div className="app">
+      React homework template
+      <p className="py-10 text-purple-700">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta ea
+        accusantium possimus. Debitis voluptatum aut repellendus soluta iusto
+        eveniet, provident reiciendis. Iusto omnis quod ad quaerat nemo delectus
+        impedit quis.
+      </p>
+    </div>
+  );
+};
